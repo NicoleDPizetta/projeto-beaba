f7d5c3fb099ab9b8ca41bc2182ef70f8a6229cbc
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-09-26 17:30:58 -0300

    ajustes no Layout Base

--- src/pages/pagina-inicial/PaginaInicial.tsx
+++ src/pages/pagina-inicial/PaginaInicial.tsx
@@ -3,7 +3,7 @@ import { LayoutBase } from "../../shared/layouts"
 
 export const PaginaInicial = () => {
     return (
-            <LayoutBase titulo="Página Inicial" header={<Header />}>
+            <LayoutBase header={<Header />}>
                 <CardTemplate />
             </LayoutBase>
     )
--- src/shared/layouts/LayoutBase.tsx
+++ src/shared/layouts/LayoutBase.tsx
@@ -1,42 +1,25 @@
 import { ReactNode } from "react";
-import { Box, Typography, useTheme } from "@mui/material";
-import { Theme } from "../themes";
+import { Box, useTheme } from "@mui/material";
 
 interface ILayoutBaseProps {
-  titulo: string;
   header: ReactNode;
   children: ReactNode;
 }
 
 export const LayoutBase: React.FC<ILayoutBaseProps> = ({
   children,
-  titulo,
   header,
 }) => {
   const theme = useTheme();
 
   return (
-    <Box height="100%" display="flex" flexDirection="column" gap={1} bgcolor={Theme.palette.background.default}>
+    <Box height="100%" display="flex" flexDirection="column" gap={1} bgcolor={theme.palette.background.default}>
       <Box role="header">{header}</Box>
 
-      <Box
-        padding={1}
-        display="flex"
-        alignItems="center"
-        height={theme.spacing(12)}
-      >
-        <Typography
-          variant="h5"
-          overflow="hidden"
-          whiteSpace="nowrap"
-          textOverflow="ellipsis"
-        >
-          {titulo}
-        </Typography>
-      </Box>
-
-      <Box flex={1} overflow="auto">
-        {children}
+      <Box display='flex' flexWrap='wrap' padding='2rem 2rem'>
+        <Box flex={1} overflow="auto">
+          {children}
+        </Box>
       </Box>
     </Box>
   );
