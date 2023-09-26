a1a2bcf08d4b8a0826da5b3aef0c97e42aba7750
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-09-26 15:02:39 -0300

    Alguns ajustes no Header

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,8 +1,16 @@
-import { Box, Button, TextField, Paper, useTheme  } from "@mui/material";
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Box, Button, TextField, Paper, useTheme } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { MenuContaUsuario } from "../menu-conta-usuario/MenuContaUsuario";
 
-export const Header: React.FC = () => {
+interface IHeaderProps {
+  textoDaBusca?: string;
+  aoMudarTextoDeBusca?: (novoTexto: string) => void;
+}
+
+export const Header: React.FC<IHeaderProps> = ({
+  textoDaBusca = "",
+  aoMudarTextoDeBusca,
+}) => {
   const theme = useTheme();
 
   return (
@@ -14,18 +22,22 @@ export const Header: React.FC = () => {
       paddingX={2}
       display="flex"
       alignItems="center"
-      /* justifyContent='space-between' */
       gap={1}
     >
+      <Box display="flex" justifyContent="start">
+          <TextField
+            size="small"
+            value={textoDaBusca}
+            onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
+            placeholder="Pesquisar"
+            type="search"
+          />
+        <Button variant="contained" color="primary" disableElevation>
+          <SearchOutlinedIcon />
+        </Button>
+      </Box>
 
-        <Box display='flex' justifyContent='start'>
-            <TextField size="small" placeholder="Pesquisar" type="search"/>
-            <Button variant="contained" color="primary" disableElevation>
-                <SearchOutlinedIcon />
-            </Button>
-        </Box>
-
-      <Box flex={1} display='flex' justifyContent='end'>
+      <Box flex={1} display="flex" justifyContent="end">
         <MenuContaUsuario />
       </Box>
     </Box>
