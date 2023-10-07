10941f9b8e623c60d323773a00397c363555cd0a
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-10-07 15:26:34 -0300

    TypeORM retirado do projeto

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,14 +1,1 @@
-import "dotenv/config";
-import "reflect-metadata";
-import { DataSource } from "typeorm";
 
-const port = process.env.db_port as number | undefined;
-
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.db_host,
-  port: port,
-  username: process.env.db_user,
-  password: process.env.db_pass,
-  database: process.env.db_name,
-});
