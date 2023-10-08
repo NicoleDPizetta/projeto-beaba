eaa138b36623233734b3fd22c021abac38689227
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-10-08 11:44:17 -0300

    CRUD! Criadas as rotas de manipulacao de Templates :tada:

---
+++ backend/src/controllers/TemplatesController.ts
@@ -0,0 +1,162 @@
+import { PrismaClient } from "@prisma/client";
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { Templates } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(bodyParser.json());
+
+export class TemplatesController {
+  async criarNovoTemplate(req: Request, res: Response) {
+    try {
+      const {
+        nome,
+        extensao,
+        colunas,
+        linhas,
+        campos,
+        status,
+        criador,
+        squad,
+      } = req.body;
+      const novoTemplate = await prisma.templates.create({
+        data: {
+          nome,
+          extensao,
+          colunas,
+          linhas,
+          campos,
+          status,
+          criador,
+          squad,
+        },
+      });
+      res.json(novoTemplate);
+    } catch (error) {
+      console.error("Erro ao criar novo template", error);
+      res.status(500).json({
+        error: "Não foi possível criar um novo template",
+      });
+    }
+  }
+
+  async consultarTemplatePorID(req: Request, res: Response) {
+    try {
+      const templateID = req.body.id;
+      const template: Templates | null = await prisma.templates.findUnique({
+        where: { id: templateID },
+      });
+      if (template) {
+        res.json(template);
+      } else {
+        res.status(404).json({
+          error: "ID do template não encontrado",
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao consultar template por ID", error);
+      res.status(500).json({
+        error: "Não foi possível consultar este ID de template",
+      });
+    }
+  }
+
+  async consultarTemplatesAtivos(req: Request, res: Response) {
+    try {
+      const templatesAtivos: Templates[] = await prisma.templates.findMany({
+        where: { status: true },
+      });
+      res.json(templatesAtivos);
+    } catch (error) {
+      console.error("Erro ao consultar todos os templates ativos", error);
+      res.status(500).json({
+        error: "Não foi possível consultar todos os templates ativos",
+      });
+    }
+  }
+
+  async consultarTemplatesInativos(req: Request, res: Response) {
+    try {
+      const templatesAtivos: Templates[] = await prisma.templates.findMany({
+        where: { status: false },
+      });
+      res.json(templatesAtivos);
+    } catch (error) {
+      console.error("Erro ao consultar todos os templates inativos", error);
+      res.status(500).json({
+        error: "Não foi possível consultar todos os templates inativos",
+      });
+    }
+  }
+
+  async excluirTemplate(req: Request, res: Response) {
+    try {
+      const templateID = req.body.id;
+      const template: Templates | null = await prisma.templates.findUnique({
+        where: { id: templateID },
+      });
+      if (!template) {
+        res.status(404).json({
+          error: "ID do template não encontrado",
+        });
+      } else {
+        await prisma.templates.delete({
+          where: { id: template.id },
+        });
+        res.json("Template deletado com sucesso");
+      }
+    } catch (error) {
+      console.error("Erro ao excluir template", error);
+      res.status(500).json({
+        error: "Não foi possível excluir este template",
+      });
+    }
+  }
+
+  async editarTemplate(req: Request, res: Response) {
+    try {
+      const templateID = req.body.id;
+      const {
+        nome,
+        extensao,
+        colunas,
+        linhas,
+        campos,
+        status,
+        criador,
+        squad,
+      } = req.body;
+
+      const template: Templates | null = await prisma.templates.findUnique({
+        where: { id: templateID },
+      });
+      if (!template) {
+        res.status(404).json({
+          error: "ID de usuário não encontrado",
+        });
+        return;
+      }
+
+      const atualizarTemplate: Templates = await prisma.templates.update({
+        where: { id: templateID },
+        data: {
+          nome,
+          extensao,
+          colunas,
+          linhas,
+          campos,
+          status,
+          criador,
+          squad,
+        },
+      });
+      res.json(atualizarTemplate);
+    } catch (error) {
+      console.error("Erro ao editar este template", error);
+      res.status(500).json({
+        error: "Não foi possível editar este template",
+      });
+    }
+  }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { UsuariosController } from "./controllers/UsuariosController";
+import { TemplatesController } from "./controllers/TemplatesController";
 
 const app = express();
 
@@ -16,6 +17,19 @@ app.get("/usuarios/:id", new UsuariosController().consultarUsuarioPorID);
 app.delete("/usuarios/:id", new UsuariosController().excluirUsuario);
 
 app.put("/usuarios/:id", new UsuariosController().editarUsuario);
+
+/* Rotas de manipulação de Templates */
+app.post("/criar-template", new TemplatesController().criarNovoTemplate);
+
+app.get("/templates/:id", new TemplatesController().consultarTemplatePorID);
+
+app.get("/templates", new TemplatesController().consultarTemplatesAtivos);
+
+app.get("/gerenciar-templates", new TemplatesController().consultarTemplatesInativos);
+
+app.delete("/templates/:id", new TemplatesController().excluirTemplate);
+
+app.put("/templates/:id", new TemplatesController().editarTemplate);
 
 app.listen(3000, () => {
   console.log("Servidor está em execução na porta 3000");
