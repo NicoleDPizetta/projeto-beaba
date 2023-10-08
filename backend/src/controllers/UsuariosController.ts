b153bc52e52bdab7347881d065ba95f7ea768387
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-10-07 21:06:22 -0300

    CRUD! Criadas as rotas de manipulacao de Usuarios :tada:

---
+++ backend/src/controllers/UsuariosController.ts
@@ -0,0 +1,145 @@
+import { PrismaClient } from "@prisma/client";
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { Usuarios } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(bodyParser.json());
+
+export class UsuariosController {
+  async criarNovoUsuarios(req: Request, res: Response) {
+    try {
+      const {
+        nome_completo,
+        nome_exibicao,
+        email,
+        matricula,
+        cargo,
+        senha,
+        permissao,
+        squad,
+      } = req.body;
+      const novoUsuario = await prisma.usuarios.create({
+        data: {
+          nome_completo,
+          nome_exibicao,
+          email,
+          matricula,
+          cargo,
+          senha,
+          permissao,
+          squad,
+        },
+      });
+      res.json(novoUsuario);
+    } catch (error) {
+      console.error("Erro ao criar novo usuário", error);
+      res.status(500).json({
+        error: "Não foi possível criar um novo usuário",
+      });
+    }
+  }
+
+  async consultarTodosUsuarios(req: Request, res: Response) {
+    try {
+      const usuarios: Usuarios[] = await prisma.usuarios.findMany();
+      res.json(usuarios);
+    } catch (error) {
+      console.error("Erro ao consultar todos os usuários", error);
+      res.status(500).json({
+        error: "Não foi possível consultar todos os usuários",
+      });
+    }
+  }
+
+  async consultarUsuarioPorID(req: Request, res: Response) {
+    try {
+      const usuarioID = req.body.id;
+      const usuarios: Usuarios | null = await prisma.usuarios.findUnique({
+        where: { id: usuarioID },
+      });
+      if (usuarios) {
+        res.json(usuarios);
+      } else {
+        res.status(404).json({
+          error: "ID de usuário não encontrado",
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao consultar usuário por ID", error);
+      res.status(500).json({
+        error: "Não foi possível consultar este ID de usuário",
+      });
+    }
+  }
+
+  async excluirUsuario(req: Request, res: Response) {
+    try {
+      const usuarioID = req.body.id;
+      const usuarios: Usuarios | null = await prisma.usuarios.findUnique({
+        where: { id: usuarioID },
+      });
+      if (!usuarios) {
+        res.status(404).json({
+          error: "ID de usuário não encontrado",
+        });
+      } else {
+        await prisma.usuarios.delete({
+          where: { id: usuarios.id },
+        });
+        res.json("Usuário deletado com sucesso");
+      }
+    } catch (error) {
+      console.error("Erro ao excluir usuário", error);
+      res.status(500).json({
+        error: "Não foi possível excluir este usuário",
+      });
+    }
+  }
+
+  async editarUsuario(req: Request, res: Response) {
+    try {
+      const usuarioID = req.body.id;
+      const {
+        nome_completo,
+        nome_exibicao,
+        email,
+        cargo,
+        senha,
+        permissao,
+        squad,
+      } = req.body;
+
+      const usuarios: Usuarios | null = await prisma.usuarios.findUnique({
+        where: { id: usuarioID },
+      });
+      if (!usuarios) {
+        res.status(404).json({
+          error: "ID de usuário não encontrado",
+        });
+        return;
+      }
+
+      const atualizarUsuario: Usuarios = await prisma.usuarios.update({
+        where: { id: usuarioID },
+        data: {
+          nome_completo,
+          nome_exibicao,
+          email,
+          cargo,
+          senha,
+          permissao,
+          squad,
+        },
+      });
+
+      res.json(atualizarUsuario)
+    } catch (error) {
+      console.error("Erro ao editar usuário", error);
+      res.status(500).json({
+        error: "Não foi possível editar este usuário",
+      });
+    }
+  }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { UsuariosController } from "./controllers/UsuariosController";
 
+const app = express();
+
+app.use(bodyParser.json());
+
+/* Rotas de manipulação de Usuários */
+app.post("/cadastrar", new UsuariosController().criarNovoUsuarios);
+
+app.get("/usuarios", new UsuariosController().consultarTodosUsuarios);
+
+app.get("/usuarios/:id", new UsuariosController().consultarUsuarioPorID);
+
+app.delete("/usuarios/:id", new UsuariosController().excluirUsuario);
+
+app.put("/usuarios/:id", new UsuariosController().editarUsuario);
+
+app.listen(3000, () => {
+  console.log("Servidor está em execução na porta 3000");
+});
