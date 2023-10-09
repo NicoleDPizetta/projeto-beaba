1be9a613009760bebde3d988c29bb6dc6c5e1582
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-10-09 15:33:11 -0300

    CORS Instalado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors"
 import bodyParser from "body-parser";
 import { UsuariosController } from "./controllers/UsuariosController";
 import { TemplatesController } from "./controllers/TemplatesController";
 
 const app = express();
 
+app.use(cors())
+app.use(express.json());
 app.use(bodyParser.json());
 
 /* Rotas de manipulação de Usuários */
