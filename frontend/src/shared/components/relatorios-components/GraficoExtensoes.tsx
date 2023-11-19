e4d6d1a6f3e8e4928245358c91f5c0089c3f9e5f
Nicole Dalzotto <nicoledalzotto@outlook.com>
2023-11-19 17:15:15 -0300

    :lipstick: Alterando estilo do grafico de extensoes

--- frontend/src/shared/components/relatorios-components/GraficoExtensoes.tsx
+++ frontend/src/shared/components/relatorios-components/GraficoExtensoes.tsx
@@ -28,30 +28,52 @@ export const GraficoExtensoes: React.FC<GraficoExtensoesProps> = ({
     parseFloat(getPercentagens(extension))
   );
 
-  const series: { name: string; data: number[] }[] = [
-    {
-      name: "Porcentagem",
-      data: data,
-    },
-  ];
+  const series: number[] = data;
 
   const options: ApexOptions = {
     chart: {
-      type: "bar",
+      height: 390,
+      type: "radialBar",
     },
+    labels: [".xlsx", ".xls", ".csv"],
     plotOptions: {
-      bar: {
-        borderRadius: 4,
-        horizontal: true,
+      radialBar: {
+        offsetY: 0,
+        startAngle: 0,
+        endAngle: 270,
+        hollow: {
+          margin: 5,
+          size: "30%",
+          background: "transparent",
+        },
+        dataLabels: {
+          name: {
+            show: false,
+          },
+          value: {
+            show: false,
+          },
+        },
       },
     },
-    dataLabels: {
-      enabled: false,
-    },
-    xaxis: {
-      categories: [".xlsx", ".xls", ".csv"],
-    },
     colors: ["#39B549", "#F2CA30", "#008D53"],
+    legend: {
+      show: true,
+      floating: true,
+      fontSize: "16px",
+      position: "left",
+      offsetX: 20,
+      offsetY: 10,
+      labels: {
+        useSeriesColors: true,
+      },
+      formatter: function (seriesName, opts) {
+        return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex] + "%";
+      },
+      itemMargin: {
+        vertical: 3,
+      },
+    },
   };
 
   return (
@@ -68,7 +90,7 @@ export const GraficoExtensoes: React.FC<GraficoExtensoesProps> = ({
       <ReactApexChart
         options={options}
         series={series}
-        type="bar"
+        type="radialBar"
         height={250}
       />
     </Box>
